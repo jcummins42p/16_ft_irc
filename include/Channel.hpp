a384e19e48ae7f85b778daec3e971087882c4aa7
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2024-11-04 16:59:43 +0100

    Slight changes in Channel class, need to work on methods

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmakagon <mmakagon@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:58:10 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/11/04 15:30:05 by mmakagon         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:56:29 by mmakagon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@
 class Channel {
 	private:
 		std::string			name;
-		std::string			key;
+		std::string			key; // probably should be hashed, so we don't hold the actual key in memory
 		std::string			topic;
 		std::vector<Client>	clients;
 		std::vector<Client>	operators;
 		size_t				clnts_limit;
 		bool				invite_only;
 		bool				topic_operators_only;
+
+		std::string	hashingFunc(const char* const str_to_hash) const;
 
 	public:
 		Channel(const std::string& in_name);
@@ -37,13 +39,13 @@ class Channel {
 		void		setTopic(const std::string& in_topic);
 		void		setKey(const std::string& in_key);
 
-		bool		addClient(const Client* const in_client);
+		bool		addClient(const Client* const in_client); // bool here and in other functions to check if an action was successfull, could be changed to exceptions
 		bool		addOperator(const Client* const in_operator);
 
 		bool		kickClient(const Client* const in_client);
 		bool		kickOperator(const Client* const in_operator);
 
-		bool		changeMode(char* in_mode);
+		bool		changeMode(const char* const in_mode, Client* in_client); // should check if the client is in operators list
 };
 
 #endif
