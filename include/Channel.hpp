633feb3511b1f0e4cca926e972175d5c44336280
mmakagon <mmakagon@c2r5s2.42prague.com>
2024-11-05 15:28:45 +0100

    Still working on a Channel

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -3,48 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmakagon <mmakagon@student.42.com>         +#+  +:+       +#+        */
+/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:58:10 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/11/04 18:59:51 by mmakagon         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:26:42 by mmakagon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
 
+#define CHANNEL_ERROR -1
+
 #include <iostream>
 #include "ircserv.hpp"
 
 class Channel {
 	private:
-		std::string				name;
-		std::string				key; // probably should be hashed, so we don't hold the actual key in memory
-		std::string				topic;
-		std::vector<Client*>	clients;
-		std::vector<Client*>	admins;
-		size_t					clnts_limit;
-		bool					invite_only;
-		bool					topic_admins_only;
+		std::string					name;
+		std::string					key; // probably should be hashed, so we don't hold the actual key in memory
+		std::string					topic;
+		std::vector<const Client*>	clients;
+		std::vector<const Client*>	admins;
+		size_t						clnts_limit;
+		bool						invite_only;
+		bool						topic_admins_only;
 
 		std::string	hashingFunc(const std::string& str_to_hash) const;
-		bool		isAdmin(const Client& in_client);
+		int			posInList(const std::vector<const Client*>& list, const Client& in_client) const;
+		bool		notAnAdmin(void) const;
 
 	public:
-		Channel(const std::string& in_name, Client& creator);
+		Channel(const std::string& in_name, const Client& creator);
 		~Channel();
 
 		std::string	getName(void) const;
 		std::string	getTopic(void) const;
 
-		bool		setTopic(const std::string& in_topic, const Client& modifier);
-		bool		setKey(const std::string& in_key, const Client& modifier);
 
-		bool		addClient(const Client& in_client, const Client& modifier); // bool here and in other functions to check if an action was successfull, could be changed to exceptions
-		bool		addAdmin(const Client& in_admin, const Client& modifier);
+		// bool here and in other functions to check if an action was successfull, could be changed to exceptions
+		bool		setTopic(const std::string& in_topic, const Client& admin);
+		bool		setKey(const std::string& in_key, const Client& admin);
 
-		bool		kickClient(const Client& in_client, const Client& modifier);
-		bool		kickAdmin(const Client& in_admin, const Client& modifier);
+		bool		addClient(const Client& in_client, const Client& admin);
+		bool		addAdmin(const Client& in_client, const Client& admin);
+
+		bool		kickClient(const Client& in_client, const Client& admin);
+		bool		kickAdmin(const Client& in_client, const Client& admin);
+
+		bool		joinChannel(const Client& in_client);
+		bool		leaveChannel(const Client& in_client);
 
 		bool		changeMode(const char* const in_mode, const Client& in_client); // should check if the client is in admins list
 };
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmakagon <mmakagon@student.42.com>         +#+  +:+       +#+        */
+/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:00:44 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/11/05 08:41:46 by mmakagon         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:27:45 by mmakagon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
 
-Channel::Channel(const std::string& in_name, Client& creator)
+/* CONSTRUCT - DESTRUCT */
+
+Channel::Channel(const std::string& in_name, const Client& creator)
 	: clnts_limit(MAX_CLIENTS), invite_only(false), topic_admins_only(false) {
 	if (in_name.empty())
 		name = "Default chat";
@@ -23,6 +25,22 @@ Channel::Channel(const std::string& in_name, Client& creator)
 }
 
 Channel::~Channel(void) {}
+
+
+/* PRIVATE METHODS */
+
+int	Channel::posInList(const std::vector<const Client*>& list, const Client& in_client) const {
+	for (size_t i = 0; i < list.size(); i++)
+		if (in_client.getSocket() == list[i]->getSocket())
+			return (i);
+	return (CHANNEL_ERROR);
+}
+
+// Maybe we should send errors to server somehow instead of outputs like cout or cerr?
+bool Channel::notAnAdmin(void) const {
+	std::cerr << "You don't have admin rights!" << std::endl;
+	return (false);
+}
 
 
 /* GETTERS */
@@ -38,26 +56,105 @@ std::string	Channel::getTopic(void) const {
 
 /* SETTERS */
 
-bool	Channel::setTopic(const std::string& in_topic, const Client& modifier) {
-	if (isAdmin(modifier)) {
+bool	Channel::setTopic(const std::string& in_topic, const Client& admin) {
+	if (topic_admins_only && posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+	if (in_topic.empty()) {
+		std::cerr << "Can't add an empty topic!" << std::endl;
+		return (false);
+	}
+	else {
 		topic = in_topic;
 		return (true);
 	}
-	else {
-		std::cerr << "This user doesn't have admin rights!" << std::endl;
-		return (false);
-	}
 }
 
-bool	Channel::setKey(const std::string& in_key, const Client& modifier) {
-	if (isAdmin(modifier)) {
+bool	Channel::setKey(const std::string& in_key, const Client& admin) {
+	if (posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+	if (in_key.empty()) {
+		std::cerr << "Can't add an empty key" << std::endl;
+		return (false);
+	}
+	else {
 		key = hashingFunc(in_key);
 		return (true);
 	}
-	else {
-		std::cerr << "This user doesn't have admin rights!" << std::endl;
+}
+
+/* ADMIN FUNCTIONS */
+
+bool	Channel::addClient(const Client& in_client, const Client& admin) {
+	if (posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+	if (posInList(clients, in_client) != CHANNEL_ERROR) {
+		std::cerr << "The client is already in the channel!" << std::endl;
 		return (false);
 	}
+	else {
+		clients.push_back(&in_client);
+		return (true);
+	}
 }
+
+bool	Channel::addAdmin(const Client& in_client, const Client& admin) {
+	if (posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+	else if (posInList(clients, in_client) == CHANNEL_ERROR) {
+		std::cerr << "The client is not in the channel!" << std::endl;
+		return (false);
+	}
+	else if (posInList(admins, in_client) != CHANNEL_ERROR) {
+		std::cerr << "The client is already an admin!" << std::endl;
+		return (false);
+	}
+	else {
+		admins.push_back(&in_client);
+		return (true);
+	}
+}
+
+bool	Channel::kickClient(const Client& in_client, const Client& admin) {
+	int	pos;
+
+	if (posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+	if (posInList(clients, in_client) == CHANNEL_ERROR) {
+		std::cerr << "The client is not in the channel!" << std::endl;
+		return (false);
+	}
+
+	pos = posInList(clients, in_client);
+	if (pos == CHANNEL_ERROR) {
+		std::cerr << "The client is not in the channel!" << std::endl;
+		return (false);
+	}
+	clients.erase(clients.begin() + pos);
+
+	pos = posInList(admins, in_client);
+	if (pos != CHANNEL_ERROR)
+		clients.erase(admins.begin() + pos);
+	return (true);
+}
+
+bool	Channel::kickAdmin(const Client& in_client, const Client& admin) {
+	int	pos;
+
+	if (posInList(admins, admin) == CHANNEL_ERROR)
+		return (notAnAdmin());
+
+	pos = posInList(admins, in_client);
+	if (pos == CHANNEL_ERROR) {
+		std::cerr << "The client is not an admin!" << std::endl;
+		return (false);
+	}
+	else {
+		admins.erase(admins.begin() + pos);
+		return (true);
+	}
+}
+
+
+/* JOIN - LEAVE */
 
 
