d95f37bae267e0e76fc29031f2cebb8a2a04a419
Peniel Yerima <pyerima@c2r6s6.42prague.com>
2024-11-29 18:32:27 +0100

    worked on PRIVMSG and join group

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -6,55 +6,67 @@
 /*   By: pyerima <pyerima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:58:10 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/11/27 14:43:22 by pyerima          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:17:01 by pyerima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
 
-#include <iostream>
+#include <string>
 #include <set>
 #include "Client.hpp"
+#include <sys/types.h>
 #include "ft_irc.hpp"
-#include <sys/socket.h>
+
+//#define MAX_CLIENTS 10
 
 class Channel {
-	private:
-		std::string					name;
-		std::string					topic;
-		unsigned int				hashed_pass;
-		std::set<const Client*>		clients;
-		std::set<const Client*>		admins;
-		size_t						clnts_limit;
-		bool						invite_only;
-		bool						topic_admins_only;
+private:
+    std::string name;
+    std::string topic;
+    unsigned int hashed_pass;
+    size_t clnts_limit;
+    bool invite_only;
+    bool topic_admins_only;
+    std::set<const Client*> clients;
+    std::set<const Client*> admins;
+    std::set<const Client*> invited_clients; // Declare the invited clients.
+    //std::set<Client*> clients;
+    ssize_t internalMessage(const Client& client, const std::string message) const;
 
-		ssize_t		internalMessage(const Client& client, const std::string message) const;
+public:
+    // Constructor
+    Channel(const std::string& in_name, const Client& creator, const std::string& password);
 
-	public:
-		Channel(const std::string& in_name, const Client& creator);
-		~Channel();
+    // Destructor
+    ~Channel();
 
-		const std::string&	getName(void) const;
-		const std::string&	getTopic(void) const;
+    // Topic management
+    const std::string& getName(void) const;
+    const std::string& getTopic(void) const;
+    bool setTopic(const std::string& in_topic, const Client& admin);
 
+    // Password management
+    bool setPass(std::string& in_pass, const Client& admin);
 
-		// bool here and in other functions to check if an action was successfull, could be changed to exceptions
-		bool		setTopic(const std::string& in_topic, const Client& admin);
-		bool		setPass(std::string& in_pass, const Client& admin);
+    // Channel management
+    bool addClient(const Client& in_client, const Client& admin);
+    bool addAdmin(const Client& in_client, const Client& admin);
+    bool kickClient(const Client& in_client, const Client& admin);
+    bool kickAdmin(const Client& in_client, const Client& admin);
 
-		bool		addClient(const Client& in_client, const Client& admin);
-		bool		addAdmin(const Client& in_client, const Client& admin);
+    // Join/Leave
+    bool joinChannel(const Client& in_client, const std::string& password); // Updated declaration.
+    bool leaveChannel(const Client& in_client);
+	void create(const Client& creator, const std::string& password); //
 
-		bool		kickClient(const Client& in_client, const Client& admin);
-		bool		kickAdmin(const Client& in_client, const Client& admin);
+    // Invite management
+    void inviteClient(const Client& in_client, const Client& admin); // Declare the inviteClient method.
 
-		bool		joinChannel(const Client& in_client);
-		bool		leaveChannel(const Client& in_client);
-
-		bool		changeMode(const char* const in_mode, const Client& admin);
-		void		channelMessage(const std::string message, const Client& sender) const;
+    // Group messaging
+    void channelMessage(const std::string message, const Client& sender) const;
+    
 };
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -3,25 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyerima <pyerima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:00:44 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/11/19 12:29:55 by mmakagon         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:44:21 by pyerima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
+#include <sys/types.h>
+#include "ft_irc.hpp"
+#include <sys/socket.h>
 
 /* CONSTRUCT - DESTRUCT */
 
-Channel::Channel(const std::string& in_name, const Client& creator)
-	: hashed_pass(0), clnts_limit(MAX_CLIENTS), invite_only(false), topic_admins_only(false) {
-	if (in_name.empty())
-		name = "Default chat";
-	else
-		name = in_name;
-	clients.insert(&creator);
-	admins.insert(&creator);
+Channel::Channel(const std::string& in_name, const Client& creator, const std::string& password)
+    : clnts_limit(MAX_CLIENTS), invite_only(false), topic_admins_only(false) {
+    if (in_name.empty())
+        name = "Default chat";
+    else
+        name = in_name;
+    
+    if (password.empty())
+        throw std::invalid_argument("Password must not be empty when creating a channel.");
+    
+    hashed_pass = hashSimple(password);  // Set the hashed password for the channel.
+    clients.insert(&creator);           // Add the creator as the first client.
+    admins.insert(&creator);            // Set the creator as the initial admin.
 }
 
 Channel::~Channel(void) {}
@@ -30,7 +38,7 @@ Channel::~Channel(void) {}
 /* PRIVATE METHODS */
 
 ssize_t Channel::internalMessage(const Client& client, const std::string message) const {
-	return (send(client.getFd(), message.c_str(), message.size(), 0));
+    return send(client.getFd(), message.c_str(), message.size(), 0);
 }
 
 
@@ -106,73 +114,141 @@ bool	Channel::addAdmin(const Client& in_client, const Client& admin) {
 	}
 }
 
-bool	Channel::kickClient(const Client& in_client, const Client& admin) {
-	if (admins.find(&admin) == admins.end()) {
-		internalMessage(admin, "You don't have admin rights!\n");
-		return (false);
-	}
-	else if (clients.find(&in_client) == clients.end()) {
-		internalMessage(admin, "Can't kick - the user is not in the channel!\n");
-		return (false);
-	}
-	else {
-		clients.erase(&in_client);
-		admins.erase(&in_client);
-		return (true);
-	}
+bool Channel::kickClient(const Client& in_client, const Client& admin) {
+    if (admins.find(&admin) == admins.end()) {
+        internalMessage(admin, "You don't have admin rights!\n");
+        return false;
+    }
+    else if (clients.find(&in_client) == clients.end()) {
+        internalMessage(admin, "Can't kick - the user is not in the channel!\n");
+        return false;
+    }
+    else if (admins.find(&in_client) != admins.end()) {
+        internalMessage(admin, "You cannot kick an admin!\n");
+        return false;  // Ensure admins cannot be kicked by non-admins
+    }
+    else {
+        clients.erase(&in_client);  // Remove the user from the channel
+        return true;
+    }
 }
 
-bool	Channel::kickAdmin(const Client& in_client, const Client& admin) {
-	if (admins.find(&admin) == admins.end()) {
-		internalMessage(admin, "You don't have admin rights!\n");
-		return (false);
-	}
-	else if (clients.find(&in_client) == clients.end()) {
-		internalMessage(admin, "Can't kick - the user is not in the channel!\n");
-		return (false);
-	}
-	else if (admins.find(&in_client) == admins.end()) {
-		internalMessage(admin, "Can't kick - the user is not an admin!\n");
-		return (false);
-	}
-	else {
-		admins.erase(&in_client);
-		return (true);
-	}
+
+bool Channel::kickAdmin(const Client& in_client, const Client& admin) {
+    if (admins.find(&admin) == admins.end()) {
+        internalMessage(admin, "You don't have admin rights!\n");
+        return false;
+    }
+    else if (clients.find(&in_client) == clients.end()) {
+        internalMessage(admin, "Can't kick - the user is not in the channel!\n");
+        return false;
+    }
+    else if (admins.find(&in_client) == admins.end()) {
+        internalMessage(admin, "Can't kick - the user is not an admin!\n");
+        return false;
+    }
+    else {
+        admins.erase(&in_client);  // Remove the user from the admin list
+
+        // If all admins are removed, assign a new admin if there are clients left.
+        if (admins.empty() && !clients.empty()) {
+            // Assign the first client as the new admin
+            admins.insert(*clients.begin());
+            internalMessage(**clients.begin(), "You are now the channel admin.\n");
+        }
+
+        return true;
+    }
 }
+
 
 
 /* JOIN - LEAVE */
 
-bool	Channel::joinChannel(const Client& in_client) {
-	if (invite_only) {
-		internalMessage(in_client, "Sorry, the chat is invite-only!\n");
-		return (false);
-	}
-	else if (clients.size() >= clnts_limit) {
-		internalMessage(in_client, "Sorry, the chat is full!\n");
-		return (false);
-	}
-	else {
-		clients.insert(&in_client);
-		return (true);
-	}
+bool Channel::joinChannel(const Client& in_client, const std::string& password) {
+    // Check if the channel is invite-only and if the client is invited.
+    if (invite_only && invited_clients.find(&in_client) == invited_clients.end()) {
+        internalMessage(in_client, "Sorry, the chat is invite-only!\n");
+        return false;
+    }
+
+    // If the channel isn't invite-only, check the password.
+    if (!invite_only) {
+        unsigned int hashed_attempt = hashSimple(password);
+        if (hashed_attempt != hashed_pass) {
+            internalMessage(in_client, "Incorrect password!\n");
+            return false;
+        }
+    }
+
+    // If the channel is full, deny the client.
+    if (clients.size() >= clnts_limit) {
+        internalMessage(in_client, "Sorry, the chat is full!\n");
+        return false;
+    }
+
+    // Add the client to the channel.
+    clients.insert(&in_client);
+    return true;
 }
 
-bool	Channel::leaveChannel(const Client& in_client) {
-	admins.erase(&in_client);
-	if (admins.empty()) {
-		if (!clients.empty())
-			admins.insert(*clients.begin());
-	}
-	clients.erase(&in_client);
-	return (true);
+// // Function to create a new channel
+// void Channel::create(const Client& creator, const std::string& password) {
+//     // This function initializes the channel with a password and sets the creator as the admin
+//     hashed_pass = hashSimple(password);  // Store hashed password
+//     clients.insert(&creator);            // Add the creator to the channel
+//     admins.insert(&creator);             // Set creator as an admin
+// }
+
+
+bool Channel::leaveChannel(const Client& in_client) {
+    admins.erase(&in_client);  // Remove admin rights if applicable.
+    clients.erase(&in_client); // Remove the client from the channel.
+
+    // Reassign admin rights if the last admin leaves:
+    if (admins.empty() && !clients.empty()) {
+        admins.insert(*clients.begin());  // Assign the first client as the new admin
+        internalMessage(**clients.begin(), "You are now the channel admin.\n");  // Correctly pass the reference
+    }
+    
+    // If there are no clients left, you can clean up or close the channel.
+    if (clients.empty()) {
+        // Clean up the channel or leave it open for future clients (optional logic)
+        // Example: delete the channel, or make it inactive
+    }
+
+    return true;
+}
+
+
+
+void Channel::create(const Client& creator, const std::string& password) {
+    hashed_pass = hashSimple(password);  // Set hashed password for the channel
+    clients.insert(&creator);            // Add the creator to the clients
+    admins.insert(&creator);             // Set creator as the admin
+    // Other initialization logic if needed...
 }
 
 /* GROUP MESSAGES */
 
-void	Channel::channelMessage(const std::string message, const Client& sender) const {
-	for (std::set<const Client*>::iterator it = clients.begin(); it != clients.end(); ++it)
-		if (*it != &sender)
-			send((*it)->getFd(), message.c_str(), message.size(), 0);
+void Channel::channelMessage(const std::string message, const Client& sender) const {
+    for (std::set<const Client*>::iterator it = clients.begin(); it != clients.end(); ++it) {
+        // Don't send the message back to the sender
+        if (*it != &sender) {
+            send((*it)->getFd(), message.c_str(), message.size(), 0);
+        }
+    }
+}
+
+
+/* INVITE MANAGEMENT */
+
+void Channel::inviteClient(const Client& in_client, const Client& admin) {
+    if (admins.find(&admin) == admins.end()) {
+        internalMessage(admin, "You don't have admin rights!\n");
+        return;
+    }
+    
+    invited_clients.insert(&in_client);  // Add the client to the invited list.
+    internalMessage(in_client, "You have been invited to join the channel: " + name + "\n");
 }
